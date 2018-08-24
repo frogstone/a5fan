13e6abf0a326d698387ed2c2d0e4e7218066e48a
Yosuke Yamauchi <info@frogstone.jp>
2018-08-24 18:55:39 +0900

    hook

--- public/hook.php
+++ public/hook.php
@@ -7,7 +7,7 @@ if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payloa
     $payload = json_decode($_POST['payload'], true);
 
     if ($payload['ref'] === 'refs/heads/master') {
-        `cd /frogstone/a5fan; git pull origin master`;
+        `cd /var/www/html/online_english.club; git pull origin master`;
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
