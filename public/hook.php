7d24fb0eafaa54af76af3122a923fe9636a17b17
Yosuke Yamauchi <info@frogstone.jp>
2018-08-27 12:38:57 +0900

    t

--- public/hook.php
+++ public/hook.php
@@ -1,6 +1,28 @@
 <?php
 
 /*
+$LOG_FILE = dirname(__FILE__).'/hook.log';
+$SECRET_KEY = 'frogstone';
+
+if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
+    $payload = json_decode($_POST['payload'], true);
+
+    if ($payload['ref'] === 'refs/heads/master') {
+        `cd /var/www/html/online_english.club; git pull origin master`;
+        file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
+    }
+} else {
+    file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." invalid access: ".$_SERVER['REMOTE_ADDR']."\n", FILE_APPEND|LOCK_EX);
+}
+
+*/
+
+?>
+
+
+<?php
+
+/*
  * ▼ このファイルの使い方
  * GitHubの Settings > Webhooks に以下のように設定する
  * Payload URL:     http://xxxxx.heteml.net/_hook.php
