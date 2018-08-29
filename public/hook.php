007d7ed3a775c472a1ccb78be9d0b3f75310b1e9
Yosuke Yamauchi <info@frogstone.jp>
2018-08-29 23:26:08 +0900

    d

--- public/hook.php
+++ public/hook.php
@@ -4,7 +4,7 @@ chdir("/var/www/html/online_english.club");
 //exec('git pull origin master 2>&1',$output,$return);
 exec('git pull origin master',$output,$return);
 var_dump($output,$return);
-die("ok ");
+die("okk ");
 exit;
 #exit;a
 
