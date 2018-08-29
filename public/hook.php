8a40638c79d4875009953730f7439d8dda5c36f2
Yosuke Yamauchi <frogstone@yy.local>
2018-08-29 18:13:08 +0900

    hook

--- public/hook.php
+++ public/hook.php
@@ -62,14 +62,17 @@ print(__LINE__." ");
             # 各サイトのブランチフォルダに移動
             //chdir($payload['repository']['name'].'/'.$branch);
             //chdir('../');
+			  /*
 			  if(!chdir("/var/www/html/online_english.club/.git")) {
 				  print("failed chdir ");
 			  }
+			  */
 			  
 			  # pull実行
 			  //exec('git pull origin '.$branch.' 2>&1', $output, $return);
 			  //exec('git pull origin '.$branch.' 2>&1');
-			  exec('git pull origin '.$branch,$output,$return);
+			  //exec('git pull origin '.$branch,$output,$return);
+			  exec('/var/www/html/online_english.club/.git pull',$output,$return);
 			  print('git pull origin '.$branch);
 			  
 			  
