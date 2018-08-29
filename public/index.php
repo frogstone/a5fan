79f2d974590e7b89465e191f018ff3f2b43e7c76
Yosuke Yamauchi <info@frogstone.jp>
2018-08-30 01:39:15 +0900

    a

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Fuel is a fast, lightweight, community driven PHP5 framework
+ * Fuel is a fast, lightweight, community driven PHP5 framework a
  *
  * @package    Fuel
  * @version    1.8
