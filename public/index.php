aa06588eea781f703cc148636efce49f275d33c2
Yosuke Yamauchi <frogstone@yy.local>
2018-08-29 17:23:49 +0900

    ccc

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Fuel is a fast, lightweight, community driven PHP5 framework.aaabbb
+ * Fuel is a fast, lightweight, community driven PHP5 framework.aaabbbccc
  *
  * @package    Fuel
  * @version    1.8
